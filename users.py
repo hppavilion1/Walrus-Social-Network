5cc07437bf0c8a6c818f13d2c63ae7f75624fa5b
hppavilion1 <nathantronnes@gmail.com>
2015-06-03 06:19:41 -0800

    Age Barrier

--- users.py
+++ users.py
@@ -2,15 +2,23 @@ from sanitize import *
 from passlib.hash import sha256_crypt
 from datetime import date
 
+
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
 class user:
     def __init__(self, username, name, password, (birthyear, birthmonth, birthdate), gender):
-        self.name = sanitize(name)
-        self.username = santize(username)
-        self.password = hash = sha256_crypt.encrypt(password)
-        self.birthyear = birthyear
-        self.birthmonth = birthmonth
-        self.birthday = birthday
-        self.gender = gender
+        if calculate_age(datetime.date(birthyear, birthmonth, birthdate))>=13:
+            self.name = sanitize(name)
+            self.username = santize(username)
+            self.password = hash = sha256_crypt.encrypt(password)
+            self.birthyear = birthyear
+            self.birthmonth = birthmonth
+            self.birthday = birthday
+            self.gender = gender
+        else:
+            self = None
         
     def __str__(self):
         return(self.username+';'+self.password+';'+self.name+self.birthyear+'-'+self.birthmonth+'-'+self.birthday+';'+self.gender)
